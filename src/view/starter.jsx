f77626299b1e79c36ae1f3c113e250caa3c79039
Dilson Júnior <dilsonjlrjr@gmail.com>
2020-03-16 15:26:47 -0400

    feat (template): view starter created

---
+++ src/view/starter.jsx
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+
+// import { Container } from './styles';
+
+const Starter = () => (
+    <Fragment>        
+    </Fragment>
+);
+
+export default Starter;
