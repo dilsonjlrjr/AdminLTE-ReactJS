7166a9f2078aefb0b1b9d0bd53f82cd563a994d0
Dilson Júnior <dilsonjlrjr@gmail.com>
2020-03-16 15:25:43 -0400

    project (v1): router defined

---
+++ src/router/router_general.jsx
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+// import { Container } from './styles';
+
+const RouterGeneral = () => (
+    <Fragment>
+        <Router>
+            <Route path="/">
+                
+            </Route>
+        </Router>
+    </Fragment>
+);
+
+export default RouterGeneral;
